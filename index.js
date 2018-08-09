0325da4c407ab2053ac07ef0d659fc257c9672f0
Colin McNeil <colinmcneil@gmail.com>
2018-08-09 17:25:58 -0400

    Create logger, setup for production

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-var uuid = require('uuid4');
 const tplink = require('tplink-cloud-api')
-console.log(uuid())
+const compression = require('compression')
+const winston = require('winston')
+console.log(process.env.NODE_ENV)
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// 
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+app.use(compression())
+
 
 app.use('/', express.static('page'))
 
@@ -14,12 +37,12 @@ async function tpLink(action) {
   else await myTPLink.getLB100("Lampy").transition_light_state(0, 0)
 }
 app.get('/on', (req, res) => {
-  console.log(req)
+  logger.info(JSON.stringify(req.headers))
   tpLink('on')
 })
 app.get('/off', (req, res) => {
-  console.log(req)
+  logger.info(JSON.stringify(req.headers))
   tpLink('off')
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Started.'))
